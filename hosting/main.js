bf5f249426be56b80d21c66e9d251cd056ec432b
jonnilundy <jonathan.lundy@liferay.com>
2017-04-12 15:21:31 -0700

    rm tutorial code

--- hosting/main.js
+++ hosting/main.js
@@ -1,40 +1,15 @@
 // Insert create user method bellow
-var auth = WeDeploy.auth('auth.tutorial-auth-web.wedeploy.me');
 
-function submitForm() {
-	auth.createUser({
-		email: user.email.value,
-		name: user.name.value,
-		password: user.password.value
-	})
-	.then(function() {
-		alert('Account successfully created!');
-		signIn();
-		user.reset();
-	})
-	.catch(function() {
-		alert('Sign-up failed. Try another email.');
-		user.reset();
-	});
-}
 // Insert create user method above
 
 
 // Insert sign-in method bellow
-function signIn() {
-	auth.signInWithEmailAndPassword(user.email.value, user.password.value)
-	.then(function() {
-		document.location.href = '/welcome.html';
-	})
-	.catch(function() {
-		alert('Sign-in failed. Try another email/password.');
-	});
-}
+
 // Insert sign-in method above
 
 
 // Insert current user selector bellow
-document.querySelector('.username').innerHTML = auth.currentUser.name;
+
 // Insert current user selector above
 
 
